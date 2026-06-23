14e753899e63d9afde1908a9dc39cd836c6b6972
audaro <77943480+audaro@users.noreply.github.com>
2026-06-23 10:54:17 -0700

    build(android): link libfreetype.so at 16 KB max-page-size too (the last 4 KB straggler)

--- 4_buildlibs.sh
+++ 4_buildlibs.sh
@@ -21,7 +21,7 @@ if [[ "$error_code" -ne 0 ]]; then
   exit $error_code
 fi
 
-CFLAGS=-fno-rtti CXXFLAGS=-fno-rtti make -j4
+CFLAGS=-fno-rtti CXXFLAGS=-fno-rtti LDFLAGS="-Wl,-z,max-page-size=16384" make -j4
 make install
 
 fi
